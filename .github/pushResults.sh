480f1ea7945ea345faf0fa721531128468e83a43
krmax44 <hi@krmax44.de>
2020-12-10 15:04:05 +0100

    fix: global git settings

--- .github/pushResults.sh
+++ .github/pushResults.sh
@@ -2,8 +2,8 @@
 
 DATE=$(date '+%F')
 
-git config --local user.email "action@github.com"
-git config --local user.name "GitHub Action"
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
 
 git -C results/ add .
 git -C results/ commit -m "Reports from $DATE" -a
