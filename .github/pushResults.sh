5db6d3a6b17bb7fc076350de13ef6d51907e7e39
krmax44 <hi@krmax44.de>
2020-12-10 13:50:08 +0100

    ci: debug test run

--- .github/pushResults.sh
+++ .github/pushResults.sh
@@ -4,11 +4,14 @@ DATE=$(date '+%F')
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 
+git -C results/ status
+git -C results/ remote -v
+exit
 git -C results/ add .
 git -C results/ commit -m "Reports from $DATE" -a
 git -C results/ push origin master
 
-git add results
+git add results/
 git commit -m "ci: update reports submodule"
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git push "${remote_repo}" master
