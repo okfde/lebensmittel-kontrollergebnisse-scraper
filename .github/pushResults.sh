adfe72e81b4f4f46afc16dcd4cc48448bd36c01b
krmax44 <hi@krmax44.de>
2020-12-10 13:52:00 +0100

    ci: debug run 2

--- .github/pushResults.sh
+++ .github/pushResults.sh
@@ -4,6 +4,9 @@ DATE=$(date '+%F')
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 
+git submodule init 
+git submodule update
+
 git -C results/ status
 git -C results/ remote -v
 exit
